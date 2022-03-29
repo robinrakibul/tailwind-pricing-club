22448a1ee8f9364ebecae75dbb21a84e05a80fd8
robinrakibul <robinrakibulhassan@gmail.com>
2022-03-29 13:28:42 +0600

    added responsive menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from '../Link/Link';
+import { MenuIcon, XIcon } from '@heroicons/react/solid';
 
 const Navbar = () => {
+    const[open,setOpen] = useState(false);
     const routes = [
         {id: 1, name: 'Home', link: '/home'},
         {id: 1, name: 'Shop', link: '/shop'},
@@ -11,7 +13,10 @@ const Navbar = () => {
     ]
     return (
         <nav>
-            <ul className='md:flex justify-center'>
+            <div onClick={()=> setOpen(!open)} className='w-6 h-6 md:hidden'>
+                {open? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
+            </div>
+            <ul className={`md:flex justify-center absolute duration-500 ease-in ${open? 'top-6' : 'top-[-120px]'}`}>
                 {
                     routes.map(route=> <Link key={route.id} route={route}></Link>)
                 }
